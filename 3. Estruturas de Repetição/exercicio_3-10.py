3e70eff9d31998c071ab4ad78e213f17370f3ca3
cavalcantevitor <78443051+cavalcantevitor@users.noreply.github.com>
2023-10-23 11:32:11 -0300

    Corrigindo erro no código que estava causando o resultado vir incorretamente

--- 3. Estruturas de Repetição/exercicio_3-10.py
+++ 3. Estruturas de Repetição/exercicio_3-10.py
@@ -1,17 +1,28 @@
+# Obtendo input do usuário
 n = int(input())
+
+# Definindo variáveis
 m = 1
-# key = True
 numero = n * m + 1
 divisor = 2
-primo = True
+primo = True # definindo a propriedade primo como verdadeira
 
+# Iniciando loop para obter "m"
 while primo:
+
+    # Validando se número realmente é primo
     if (numero % divisor == 0):
         primo = False
+
+        # Encerrando o loop caso o número seja primo
+        break
+
+    # Somando unidades ao divisor
     elif (numero % divisor != 0):
         while (divisor < numero):
             divisor = divisor + 1
 
     m = m + 1
 
+# Imprimindo "m" na tela
 print(m)
